397b1639f5a391a7c851e46e3a163bb29798102e
Ayush <ayushshivampandey@gmail.com>
2024-09-26 18:11:54 +0530

    Refactor database schema and tables

--- prisma/migrations/20240926092914_newly_upgradated_migration/migration.sql
+++ prisma/migrations/20240926092914_newly_upgradated_migration/migration.sql
@@ -19,7 +19,20 @@ CREATE TABLE "Session" (
 
 -- CreateTable
 CREATE TABLE "Settings" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "shop" TEXT,
     "name" TEXT NOT NULL,
     "description" TEXT
 );
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "customerId" TEXT,
+    "productId" TEXT,
+    "shop" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Settings_shop_key" ON "Settings"("shop");
